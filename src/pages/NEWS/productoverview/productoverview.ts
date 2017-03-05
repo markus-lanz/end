b0aaab7105bcf6dd40bfba57274de7c5786a3e18
Artur <test@test.de>
2017-03-05 11:00:34 +0100

    Startseite

--- src/pages/NEWS/productoverview/productoverview.ts
+++ src/pages/NEWS/productoverview/productoverview.ts
@@ -31,26 +31,42 @@ export class ProductoverviewPage {
   productS = null;
   productT = null;
   productV = null;
+  q = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public ut:UtilityService) {
-  this.productCategory = ut.getResultArray();
-  console.log(this.productCategory)
-   this.productA = this.productCategory['A'];
-  this.productB = this.productCategory['B'];
-  this.productC = this.productCategory['C'];
-  this.productD = this.productCategory['D'];
-  this.productF = this.productCategory['F'];
-  this.productG = this.productCategory['G'];
-  this.productH = this.productCategory['H'];
-  this.productL = this.productCategory['L'];
-  this.productM = this.productCategory['M'];
-  this.productN = this.productCategory['N'];
-  this.productO = this.productCategory['O'];
-  this.productR = this.productCategory['R'];
-  this.productS = this.productCategory['S'];
-  this.productT = this.productCategory['T'];
-  this.productV = this.productCategory['V'];
-  console.log(this.productA);
+    this.productCategory = ut.getResultArray();
+    this.productA = this.productCategory['A'];
+    this.productB = this.productCategory['B'];
+    this.productC = this.productCategory['C'];
+    this.productD = this.productCategory['D'];
+    this.productF = this.productCategory['F'];
+    this.productG = this.productCategory['G'];
+    this.productH = this.productCategory['H'];
+    this.productL = this.productCategory['L'];
+    this.productM = this.productCategory['M'];
+    this.productN = this.productCategory['N'];
+    this.productO = this.productCategory['O'];
+    this.productR = this.productCategory['R'];
+    this.productS = this.productCategory['S'];
+    this.productT = this.productCategory['T'];
+    this.productV = this.productCategory['V'];
+    this.q.push(
+        { letter : 'A', items : this.productCategory['A'] },
+        { letter : 'B', items : this.productCategory['B'] },
+        { letter : 'C', items : this.productCategory['C'] },
+        { letter : 'D', items : this.productCategory['D'] },
+        { letter : 'F', items : this.productCategory['F'] },
+        { letter : 'G', items : this.productCategory['G'] },
+        { letter : 'H', items : this.productCategory['H'] },
+        { letter : 'L', items : this.productCategory['L'] },
+        { letter : 'M', items : this.productCategory['M'] },
+        { letter : 'N', items : this.productCategory['N'] },
+        { letter : 'O', items : this.productCategory['O'] },
+        { letter : 'R', items : this.productCategory['R'] },
+        { letter : 'S', items : this.productCategory['S'] },
+        { letter : 'T', items : this.productCategory['T'] },
+        { letter : 'V', items : this.productCategory['V'] },
+    )
   }
 
 
